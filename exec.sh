26c380894e51829c2273efd022123cd35e405a78
Yuki Abe <12268087+yukia3@users.noreply.github.com>
2020-09-15 18:22:23 +0900

    Fixed file name error.

--- exec.sh
+++ exec.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -eu
 echo "$(date) [start] python3 importer.py --project $1 --storagebucket $2 --workbucket $3 --dataset $4 --tdate $5 [start]"
-python3 /var/dataflow/ndimporter.py --project $1 --storagebucket $2 --workbucket $3 --dataset $4 --tdate $5
-echo "$(date) [end] python3 ndimporter.py --project $1 --storagebucket $2 --workbucket $3 --dataset $4 --tdate $5 [end]"
+python3 /var/dataflow/importer.py --project $1 --storagebucket $2 --workbucket $3 --dataset $4 --tdate $5
+echo "$(date) [end] python3 importer.py --project $1 --storagebucket $2 --workbucket $3 --dataset $4 --tdate $5 [end]"
